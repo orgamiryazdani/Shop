b720227576dae8fd26519102bd1716e2db2c0a9c
Amir <100785957+orgamiryazdani@users.noreply.github.com>
2022-07-20 12:19:15 +0430

    Update data.js

--- src/data.js
+++ src/data.js
@@ -10,7 +10,8 @@ export const products = [
     price: 120,
     offPrice: 120,
     discount: 0,
-    image: "https://s4.uupload.ir/files/item1_soj.jpg",
+    image:
+      "https://img.joomcdn.net/61a6431a96aa1404b240086516fd2ce490108d30_original.jpeg",
   },
   {
     id: 2,
@@ -19,7 +20,8 @@ export const products = [
     price: 110,
     offPrice: 100,
     discount: 8,
-    image: "https://s4.uupload.ir/files/item2_fc5s.jpg",
+    image:
+      "https://sitegenesis.production.deckers.coremedia.cloud/resource/image/43462/landscape_ratio4x3/540/405/c0801be9c69399ab41a00508f4fb9186/977A8AAD9884FEC4163B8192816E4386/full-gtx-3x.jpg",
   },
   {
     id: 3,
@@ -32,7 +34,8 @@ export const products = [
     price: 99,
     offPrice: 89,
     discount: 10,
-    image: "https://s4.uupload.ir/files/item3_zvc9.jpg",
+    image:
+      "https://www.cdn.sarabara.com/wp-content/uploads/2020/05/%DA%A9%D9%81%D8%B4-%DA%86%D8%B1%D9%85-%D9%85%D8%B1%D8%AF%D8%A7%D9%86%D9%87-%D8%A8%D9%86%D8%AF-%D8%AF%D8%A7%D8%B1-%D9%85%D8%AD%DB%8C%D8%B7%DB%8C-450x562.jpg",
   },
   {
     id: 4,
@@ -45,7 +48,8 @@ export const products = [
     price: 260,
     offPrice: 220,
     discount: 15,
-    image: "https://s4.uupload.ir/files/item4_zr94.jpg",
+    image:
+      "https://files.namnak.com/users/nh/aup/202105/4314_pics/%D8%B1%D9%88%D8%B4-%D8%B4%D8%AA%D8%B4%D9%88%DB%8C-%DA%A9%D9%81%D8%B4-%D9%88%D8%B1%D8%B2%D8%B4%DB%8C.jpg",
   },
   {
     id: 5,
@@ -58,7 +62,8 @@ export const products = [
     price: 150,
     offPrice: 150,
     discount: 0,
-    image: "https://s4.uupload.ir/files/item5_24ye.jpg",
+    image:
+      "https://shahparshoes.com/wp-content/uploads/2020/11/%DA%A9%D9%81%D8%B4-%D9%85%D8%B1%D8%AF%D8%A7%D9%86%D9%87.jpg",
   },
   {
     id: 6,
@@ -71,6 +76,7 @@ export const products = [
     price: 220,
     offPrice: 200,
     discount: 13,
-    image: "https://s4.uupload.ir/files/item6_7jfy.jpg",
+    image:
+      "http://hayatkhalvat.com/wp-content/uploads/2016/10/%DA%A9%D9%81%D8%B4-%D8%A7%D8%B3%D9%BE%D8%B1%D8%AA-4.jpg",
   },
 ];
